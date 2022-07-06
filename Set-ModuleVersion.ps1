0407c42ff0792f75adb1e040a76b407cbe3d062b
Ken Muse <kenmuse@users.noreply.github.com>
2022-07-06 23:47:58 +0000

    Cleanup modules

---
+++ Set-ModuleVersion.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 'Latest'
+#Requires -Version 5.0
+
+function Set-ModuleVersion {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")]
+    param
+    (
+        [string] $PsdPath,
+        [string] $VersionNumber,
+        [switch] $Force
+    )
+
+    process {
+        $manifest = Import-PowerShellDataFile $PsdPath
+        [version]$version = $manifest.ModuleVersion
+        if ($Force -or $PSCmdlet.ShouldProcess($PsdPath, "Update manifest from $version to $VersionNumber")){
+            Write-Information "Updating version from $version to $VersionNumber"
+            [version]$newVersion = $VersionNumber
+
+            # Update the manifest file
+            try {
+                (Get-Content $file) -replace $version, $newVersion | Set-Content $file -Encoding string
+            }
+            catch {
+                Write-Error "Failed to update the module: $_"
+            }
+        }
+    }
+}
